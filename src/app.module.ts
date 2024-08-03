dc6de028f41d1493832a93ff95c5ddcfc5b6b20e
Cauan Diniz <cauanrodiniz@gmail.com>
2024-08-02 21:25:59 -0300

    refactor: app module imports from file, meals and categories

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,21 @@ import { AuthModule } from './app/auth/auth.module';
 import { CryptoModule } from './infra/crypto/Crypto.module';
 import { JwtModule } from './infra/jwt/Jwt.module';
 import { PrismaModule } from './infra/prisma/prisma.module';
+import { FileModule } from './app/file/file.module';
+import { MealsModule } from './app/meals/meals.module';
+import { CategoriesModule } from './app/categories/categories.module';
 
 @Module({
-  imports: [UserModule, AuthModule, CryptoModule, JwtModule, PrismaModule],
+  imports: [
+    UserModule, 
+    AuthModule, 
+    CryptoModule, 
+    FileModule,
+    JwtModule, 
+    PrismaModule,
+    CategoriesModule,
+    MealsModule
+  ],
   controllers: [],
   providers: [],
 })
