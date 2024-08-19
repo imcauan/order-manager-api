54d89180bb08c0b37cc5c813060d9d0d5d012287
Cauan Diniz <cauanrodiniz@gmail.com>
2024-08-19 15:43:16 -0300

    feat: add serve static module for uploads.

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,17 +7,29 @@ import { PrismaModule } from './infra/prisma/prisma.module';
 import { FileModule } from './app/file/file.module';
 import { MealsModule } from './app/meals/meals.module';
 import { CategoriesModule } from './app/categories/categories.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { TableModule } from './app/tables/tables.module';
+import { OrderModule } from './app/orders/order.module';
 
 @Module({
   imports: [
-    UserModule, 
-    AuthModule, 
-    CryptoModule, 
+    UserModule,
+    AuthModule,
+    CryptoModule,
     FileModule,
-    JwtModule, 
+    JwtModule,
     PrismaModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'storage'),
+      serveStaticOptions: {
+        index: false,
+      },
+    }),
     CategoriesModule,
-    MealsModule
+    MealsModule,
+    TableModule,
+    OrderModule,
   ],
   controllers: [],
   providers: [],
